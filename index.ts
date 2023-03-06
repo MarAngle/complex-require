6afb3e74b1d9b4995e9df248dc5e82ac2c24c22e
Shen GuoQing <yinweiyingzhe@126.com>
2023-03-06 16:35:38 +0800

    输出对象

---
+++ index.ts
@@ -0,0 +1,11 @@
+import config from './config'
+import Require from './src/Require'
+import RequireRule from './src/RequireRule'
+import TokenRule from './src/TokenRule'
+
+export {
+  config,
+  Require,
+  RequireRule,
+  TokenRule
+}
